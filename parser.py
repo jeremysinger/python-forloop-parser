f9e711c86e3293fd6c86a8f297b1a0794609a9e4
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2019-05-29 17:21:01 +0100

    tracking for loops and ranges now - reporting numbers to stdout

--- parser.py
+++ parser.py
@@ -2,6 +2,8 @@ import ast
 import sys
 
 def main():
+    numForLoops = 0
+    numRangeLoops = 0
     if len(sys.argv) < 2:
         print('usage: parser.py file_to_analyse')
         sys.exit(-1)
@@ -10,12 +12,16 @@ def main():
         tree = ast.parse(f.read())
     for node in ast.walk(tree):
         if isinstance(node, (ast.For)):
-            print('for loop on line %d' % (node.lineno))
+            # print('for loop on line %d' % (node.lineno))
+            numForLoops += 1
             if isinstance(node.iter, (ast.Call)):
                 call = node.iter
                 if isinstance(call.func, (ast.Name)):
-                    print('for loop iter is fn call: %s' % call.func.id)
-    
+                    if call.func.id == 'range':
+                        # print('for loop iter is fn call: %s' % call.func.id)
+                        numRangeLoops += 1
+                        
+    print('for loops %d\nrange %d\n' % (numForLoops, numRangeLoops))
 
 main()
 
