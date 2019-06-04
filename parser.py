dbf3d2f09f0c6f9b5c34241e9bcbbf093a35ed71
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2019-06-04 15:16:41 +0100

    refactored code - extract function for parsing a block of code as a string

--- parser.py
+++ parser.py
@@ -1,15 +1,10 @@
 import ast
 import sys
 
-def main():
+def analyse_block(codestring):
     numForLoops = 0
     numRangeLoops = 0
-    if len(sys.argv) < 2:
-        print('usage: parser.py file_to_analyse')
-        sys.exit(-1)
-    filename = sys.argv[1]
-    with open(filename) as f:
-        tree = ast.parse(f.read())
+    tree = ast.parse(codestring)
     for node in ast.walk(tree):
         if isinstance(node, (ast.For)):
             # print('for loop on line %d' % (node.lineno))
@@ -22,6 +17,15 @@ def main():
                         numRangeLoops += 1
                         
     print('for loops %d\nrange %d\n' % (numForLoops, numRangeLoops))
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('usage: parser.py file_to_analyse')
+        sys.exit(-1)
+    filename = sys.argv[1]
+    with open(filename) as f:
+       analyse_block(f.read())
 
 main()
 
