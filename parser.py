52abf4b32e1f2677c7118c28a4ba1ff38a79e630
Jeremy Singer <jeremy.singer@glasgow.ac.uk>
2019-06-05 23:37:36 +0100

    refactoring to make cleaner analysis for cells, taken out print debug stmts

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
 import ast
 import json
 import sys
+import traceback
 
 def analyse_block(codestring):
     '''count how many for loops are present in a code block'''
     '''return a pair for (number of for loops, number of for loops with range iterator'''
+    #print('code is: ', codestring)
     numForLoops = 0
     numRangeLoops = 0
     tree = ast.parse(codestring)
@@ -45,7 +47,28 @@ def analyse_loop_depth(codestring):
     v.visit(tree)
     #print('max depth of for loop is %d' % v.maxDepth)
     return v.maxDepth
-    
+
+
+def process_single_cell(cell):
+    #print(cell)
+    if cell['cell_type'] == 'code':
+        # for each code block, run analyse_block and
+        # sum results for counts
+        # remove Jupyter % directive lines
+        if 'source' in cell:
+            listing = 'source'
+        elif 'input' in cell:
+            listing = 'input'
+        else:
+            listing = None
+        python_code_lines = [ line for line in cell[listing] if not (line.startswith("%") or line.startswith("!")) ]
+        code = ''.join(python_code_lines)
+        tmp_pair = analyse_block(code)
+        tmp = analyse_loop_depth(code)
+        return (tmp_pair[0], tmp_pair[1], tmp)
+    else:
+        return (0,0,0)
+
     
 def main():
     if len(sys.argv) < 2:
@@ -56,33 +79,43 @@ def main():
     numLoops = 0
     numRange = 0
     maxDepth = 0
-    
-    with open(filename) as f:
-        if filename.endswith(".py"):
-            # it's a Python source code file
-            code = f.read()
-            (numLoops, numRange) = analyse_block(code)
-            maxDepth = analyse_loop_depth(code)
-        elif filename.endswith(".ipynb"):
-            # it's a Jupyter notebook file
-            # open notebook with JSON
-            notebook = json.load(f)
-            # iterate over blocks, looking for code blocks
-            for cell in notebook['cells']:
-                if cell['cell_type'] == 'code':
-                    # for each code block, run analyse_block and
-                    # sum results for counts
-                    # remove Jupyter % directive lines
-                    python_code_lines = [ line for line in cell['source'] if not line.startswith("%") ]
-                    code = '\n'.join(python_code_lines)
-                    tmp_pair = analyse_block(code)
-                    numLoops += tmp_pair[0]
-                    numRange += tmp_pair[1]
-                    tmp_int = analyse_loop_depth(code)
-                    if tmp_int > maxDepth:
-                        maxDepth = tmp_int
-    # report result for analysis of file
-    print ('%s %d %d %d' % (filename, numLoops, numRange, maxDepth))
+
+    try:
+        with open(filename) as f:
+            if filename.endswith(".py"):
+                # it's a Python source code file
+                code = f.read()
+                (numLoops, numRange) = analyse_block(code)
+                maxDepth = analyse_loop_depth(code)
+            elif filename.endswith(".ipynb"):
+                # it's a Jupyter notebook file
+                # open notebook with JSON
+                notebook = json.load(f)
+                # iterate over blocks, looking for code blocks
+                if 'worksheets' in notebook:
+                    for worksheet in notebook['worksheets']:
+                        if 'cells' in worksheet:
+                            for cell in worksheet['cells']:
+                                tmp_triple = process_single_cell(cell)
+                                numLoops += tmp_triple[0]
+                                numRange += tmp_triple[1]
+                                if tmp_triple[2] > maxDepth:
+                                    maxDepth = tmp_triple[2]
+                            
+
+                elif 'cells' in notebook:
+                    for cell in notebook['cells']:
+                        tmp_triple = process_single_cell(cell)
+                        numLoops += tmp_triple[0]
+                        numRange += tmp_triple[1]
+                        if tmp_triple[2] > maxDepth:
+                            maxDepth = tmp_triple[2]
+
+        # report result for analysis of file
+        print ('%s %d %d %d' % (filename, numLoops, numRange, maxDepth))
+    except:
+        print('%s failure' % filename)
+        ####traceback.print_exc()
 
 main()
 
